f15f8d1a24af076e4fdc566693eb9626ce51b30c
VictorSilva244 <victor.silva37@outlook.com>
2023-04-10 23:33:39 -0300

    Atividade - Componente Perfume
    atividade da aula TÓPICOS ESPECIAIS EM COMPUTAÇÃO

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import './App.css'
+import { Card } from './components/Card'
+
+export function App(){                       //Nome do Componente sempre em Maiusculo
+  return (                                 //O return só pode retornar um 'conteiner'
+    <Card />
+  )
+}
---
+++ src/components/Card.jsx
@@ -0,0 +1,30 @@
+import './Card.css'
+import perfume from '../assets/image-product-desktop.jpg'
+import cartIcon from '../assets/icon-cart.svg'
+
+
+
+export function Card(){
+    return(
+        <section className='card'>
+            <img src={perfume} alt='imagem promo'/>
+
+            <section className='info'>
+                <p>P E R F U M E</p>
+                <h1>Gabrielle Essence Eau De Parfum</h1>
+                <p>A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.</p>
+                <section className='preços'>
+                    <div className='precoAtual'>$149.99</div>
+                    <div className='precoOriginal'>$169.99</div>
+                </section>
+                <button><img src={cartIcon}/> Add to Cart</button>
+            </section>
+
+            
+        </section>
+        
+    )
+
+
+
+}
